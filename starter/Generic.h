e32a7a64a65e3ecbaa278c5841e874497dd8cb0f
Msalam96 <34826407+Msalam96@users.noreply.github.com>
2018-04-27 20:46:12 -0400

    Update Generic.h

--- starter/Generic.h
+++ starter/Generic.h
@@ -16,18 +16,21 @@ Description:	Header file for generic tasks. Mostly defines how to to print
 
 class Generic : public Task {
 public:
-	//Default constrcutor that will output the instructions when ADD command is called. Sets tasktype to G.
+	/**Generic() - default constructor, calls addInstruction and sets taskType to G
+	 */
 	Generic();
-	//Overloaded constructor that is called when LOAD Command is called. function that is called using istream, 
-	//since the file to be loaded is in a specific format t goes character by character in order to determine 
-	//which specific character is which. When a "|" is found it means that a new piece of a Task is found in the line.
-	Generic(string line);
-	//prints list according to specs
+	/**Generic() - Overloaded constructor that is called when LOAD Command is called. 
+				   Function that is called using istream, since the file to be loaded 
+				   is in a specific format it goes character by character in order to 
+				   determine which specific character is which. When a "|" is found 
+				   it means that a new piece of a Task is found in the line.
+	 *@param string		line	[in]	Line read from file
+	 */
+	Generic(std::string line);
+
 	void printList();
-	//detailed list for generic task types are the same as normal print, call printlist again	
 	void detailedPrint() {printList();};
-	//Outputs the task to a text file when SAVE command is called
-	string printtoFile();
+	std::string printtoFile();
 };
 
 #endif
