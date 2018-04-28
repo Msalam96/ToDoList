2b2bd9f22732922fd7a06906ff533ff91a5fe781
Msalam96 <34826407+Msalam96@users.noreply.github.com>
2018-04-27 20:51:38 -0400

    Update Task.cpp

--- starter/Task.cpp
+++ starter/Task.cpp
@@ -1,16 +1,17 @@
 /***************************************************************
 Title:			Task.cpp
 Author:			Mohammed Salam
-Date Created:	3/29/2018
+Date Created:		3/29/2018
 Class:			Spring 2018, CSCI 235-03, Mon - Wed 7:00pm-8:15 pm
 Professor:		Aarsh Vora
 Purpose:		Assignment #2
-Description:	Cpp file for the Task class, defines all non inline
-				functions of the class. 
+Description:		Cpp file for the Task class, defines all non inline
+			functions of the class. 
 ***************************************************************/
 
 #include "Task.h"
 
+using namespace std;
 Task::Task() {
 	completed = false;
 }
